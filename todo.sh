bc0180f7e48c13a03d9c76be52c7dc8c6184cd73
Grant <gergely.daniel7@gmail.com>
2018-01-05 21:53:52 +0100

    Change .ssh/config to solve permission problem

--- todo.sh
+++ todo.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Bash to-do list
 
-#Copy necessary files
+# Copy necessary files
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp -a $DIR/. ~
 rm -rf ~/.git ~/todo.sh ~/todocat.sh ~/wakatime-key
 
 # Vim settings
-## First, download the Vundle library
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+## First, download the Vundle library if haven't already
+if [[ ! -d  ~/.vim/bundle/vundle ]] ; then git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle ; fi
 ## Install plugins
 vim -c ":PluginInstall"
 
