a1e01a8713ec523a29f89af1efca9c716623babf
Grant <gergely.daniel7@gmail.com>
2018-07-20 15:35:45 +0200

    Replace cower with yay, and fix crontab update bug

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+# COLORS YAAY
+sed -i 's/#Color/Color/g' /etc/pacman.conf
+
 # UPDATE ALL PACKAGES
 pacman -Syu
 
 # INSTALL BASIC PACKAGES
 pacman -S git vim bash-completion htop	# BASIC 
-pacman -S base-devel cower				# AUR
+pacman -S base-devel yay				# AUR
 pacman -S remmina freerdp				# RDP
 pacman -S nodejs npm					# JS
 pacman -S php composer					# PHP
+pacman -S budgie-extras					# BUDGIE
 
 # REMOVE UNNECESSARY BLOAT
 pacman -R brasero empathy gedit hexchat lollypop manjaro-hello manjaro-documentation-en transmission-gtk uget totem thunderbird tlp
 
 # INSTALL PACKAGES FROM AUR
-function cowerinstall(){ cd ; cower -d $1 ; cd $1 ; makepkg -si ; }
-cowerinstall google-chrome
-cowerinstall visual-studio-code-bin
-cowerinstall discord
-cowerinstall spotify
-cowerinstall slack-desktop
-cowerinstall jetbrains-toolbox
+yay -S google-chrome
+yay -S visual-studio-code-bin
+yay -S discord
+yay -S spotify
+yay -S slack-desktop
+yay -S jetbrains-toolbox
+yay -S budgie-screenshot-applet
+yay -S peek
--- randombg.sh
+++ randombg.sh
@@ -3,8 +3,11 @@
 # COPY TO EXECUTABLE LOCATION
 cp lib/randombg ~/bin/randombg 
 
-# ADD TO CRONTAB
-(crontab -l 2>/dev/null; echo "*/5 * * * * $HOME/bin/randombg") | crontab -
+# ADD TO CRONTAB IF NOT EXISTS
+if [[ -z $(crontab -l | grep $HOME/bin/randombg) ]] 
+then
+	( crontab -l 2>/dev/null; echo "*/5 * * * * $HOME/bin/randombg" ) | crontab -
+fi
 
 # RUN FOR RANDOM IMAGE
 $HOME/bin/randombg
