fd9128bbbb9e1e46cb0479a5513105d93501b4d2
Grant <gergely.daniel7@gmail.com>
2020-07-08 18:02:17 +0200

    Update installed packages from PC

--- install.sh
+++ install.sh
@@ -6,14 +6,15 @@ sudo sed -i 's/#Color/Color/g;s/#ILoveCandy/ILoveCandy/g' /etc/pacman.conf
 sudo pacman -Syu
 
 # INSTALL BASIC PACKAGES
-sudo pacman -S git vim bash-completion htop       # BASIC 
-sudo pacman -S base-devel yay                     # AUR
-sudo pacman -S kupfer terminator playerctl nemo   # OS USAGE
-sudo pacman -S remmina freerdp                    # RDP
-sudo pacman -S nodejs npm                         # JS
-sudo pacman -S php composer                       # PHP
-sudo pacman -S budgie-extras gnome-tweaks         # BUDGIE
-sudo pacman -S discord                            # DISCORD
+sudo pacman -S git vim bash-completion htop ncdu bat                # BASIC 
+sudo pacman -S base-devel yay                                       # AUR
+sudo pacman -S kupfer terminator playerctl nemo nemo-fileroller     # OS USAGE
+sudo pacman -S budgie-extras gnome-tweaks adapta-maia-theme         # BUDGIE
+sudo pacman -S remmina freerdp                                      # RDP
+sudo pacman -S nodejs npm                                           # JS
+sudo pacman -S php composer                                         # PHP
+sudo pacman -S discord                                              # DISCORD
+sudo pacman -S nextcloud-client                                     # NEXTCLOUD
 
 # REMOVE UNNECESSARY BLOAT
 sudo pacman -R brasero gedit hexchat hplip lollypop manjaro-hello manjaro-documentation-en manjaro-printer pidgin transmission-gtk uget totem thunderbird tlp yelp
