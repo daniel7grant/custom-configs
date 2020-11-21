dc724c5ef340b92f79d0c14c0aaadbfeaf6195d6
Grant <gergely.daniel7@gmail.com>
2020-11-21 17:46:24 +0100

    Change terminator config update packages

--- install.sh
+++ install.sh
@@ -6,24 +6,25 @@ sudo sed -i 's/#Color/Color/g;s/#ILoveCandy/ILoveCandy/g' /etc/pacman.conf
 sudo pacman -Syu
 
 # INSTALL BASIC PACKAGES
-sudo pacman -S git vim bash-completion htop ncdu bat                # BASIC 
-sudo pacman -S base-devel yay                                       # AUR
-sudo pacman -S kupfer terminator playerctl nemo nemo-fileroller     # OS USAGE
-sudo pacman -S budgie-extras gnome-tweaks adapta-maia-theme         # BUDGIE
-sudo pacman -S remmina freerdp                                      # RDP
-sudo pacman -S nodejs npm                                           # JS
-sudo pacman -S php composer                                         # PHP
-sudo pacman -S discord                                              # DISCORD
-sudo pacman -S nextcloud-client                                     # NEXTCLOUD
+sudo pacman --needed -S git vim bash-completion htop ncdu bat                              # BASIC 
+sudo pacman --needed -S base-devel yay                                                     # AUR
+sudo pacman --needed -S kupfer terminator playerctl nemo nemo-fileroller ttf-inconsolata   # OS USAGE
+sudo pacman --needed -S budgie-extras gnome-tweaks adapta-maia-theme                       # BUDGIE
+sudo pacman --needed -S remmina freerdp                                                    # RDP
+sudo pacman --needed -S nodejs npm                                                         # JS
+sudo pacman --needed -S php composer                                                       # PHP
+sudo pacman --needed -S discord                                                            # DISCORD
+sudo pacman --needed -S code                                                               # VS CODE
+sudo pacman --needed -S nextcloud-client                                                   # NEXTCLOUD
 
 # REMOVE UNNECESSARY BLOAT
 sudo pacman -R brasero gedit hexchat hplip lollypop manjaro-hello manjaro-documentation-en manjaro-printer pidgin transmission-gtk uget totem thunderbird tlp yelp
 
 # INSTALL PACKAGES FROM AUR
-yay -S google-chrome \
-       visual-studio-code-bin \
-       spotify \
-       slack-desktop \
-       jetbrains-toolbox \
-       flameshot \
-       peek
+yay --needed -S \
+	# google-chrome \
+    spotify \
+    slack-desktop \
+    # jetbrains-toolbox \
+    flameshot \
+    peek
