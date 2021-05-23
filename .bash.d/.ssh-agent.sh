116693416fa201944c8764004aad0bbc252a8aa1
Grant <gergely.daniel7@gmail.com>
2021-05-23 20:46:59 +0200

    Add ssh-agent configuration to bash.d

---
+++ .bash.d/.ssh-agent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# START SSH-AGENT IF HAVEN'T ALREADY
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+fi
+if [[ ! "$SSH_AUTH_SOCK" ]]; then
+    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+fi
+
